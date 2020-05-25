9b02d15a970aa0f9184fa4c78c791942aef2c6d1
Greg Schuman <gcschuman@hotmail.com>
2020-05-25 11:17:16 -0700

    added FontAwesome stylesheet to index.html, updated app.js, added header.js, and added history.js and search.js

---
+++ book-app/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <Header>
+            <i class="fas fa-book-open"></i>
+            <h1>Book Search</h1>
+        </Header>
+    )
+}
+
+export default App
