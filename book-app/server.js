c152d5d8a1f75df5cc92a3ddb95bbfd2ae1de970
Greg Schuman <gcschuman@hotmail.com>
2020-06-20 08:37:23 -0700

    Added the configuring for AJAX requests and serve up static assets.

--- book-app/server.js
+++ book-app/server.js
@@ -5,3 +5,12 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Configure body parsing for AJAX requests
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+
