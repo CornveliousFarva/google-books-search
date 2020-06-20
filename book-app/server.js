6ec370f9d17b277ee77bb6dfc76a64ecc047d03e
Greg Schuman <gcschuman@hotmail.com>
2020-06-20 08:39:40 -0700

    Added the connection to mongodb.

--- book-app/server.js
+++ book-app/server.js
@@ -16,5 +16,15 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://user1:password1@ds125871.mlab.com:25871/heroku_0xn0jnk7",
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  }
+);
+
+
 
 
